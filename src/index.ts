66d215aa1128d8a51b6cefa73fc2c93ce0530a4d
Lívio Lopes <antonio.lvio760@gmail.com>
2023-08-24 00:33:04 -0300

    feat(index): create instances on index

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import Battle, { PVE, PVP } from './Battle';
+import Character from './Character';
+import Dragon from './Dragon';
+import Monster from './Monster';
+
+// PLayers
+const player1 = new Character('player1');
+const player2 = new Character('player2');
+const player3 = new Character('player3');
+player1.levelUp();
+player1.levelUp();
+player1.levelUp();
+// Monsters
+const monster1: Monster = new Monster();
+const monster2: Monster = new Dragon();
+// PVP
+const pvp:PVP = new PVP(player2, player3);
+// PVE
+const pve:PVE = new PVE(player1, [monster1, monster2]);
+
+const runBattles = (battles: Battle[]) => {
+  battles.forEach((battle) => {
+    battle.fight();
+  });
+};
+
+export { 
+  player1,
+  player2,
+  player3, 
+  monster1,
+  monster2, 
+  pvp,
+  pve,
+  runBattles,
+};
